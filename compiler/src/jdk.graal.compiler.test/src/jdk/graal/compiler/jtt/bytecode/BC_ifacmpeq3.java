c9bfd7d65422aed36808f9770ab499f14306862d
MichaelHaas99 <michael.haas0612@gmail.com>
2024-09-30 18:36:33 +0200

    fixing stuff concerning substitutability check during PEA

--- compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/jtt/bytecode/BC_ifacmpeq3.java
+++ compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/jtt/bytecode/BC_ifacmpeq3.java
@@ -9,11 +9,14 @@ import jdk.graal.compiler.options.OptionValues;
 public class BC_ifacmpeq3 extends JTTTest {
 
     private static value class InlineTypeTestClass {
+        static Object escapedObject = new Object();
+        static Object escapedObject2 = escapedObject;
+
         final int x;
         final float y;
-        final InlineTypeTestClass z;
+        final Object z;
 
-        InlineTypeTestClass(int x, float y, InlineTypeTestClass z) {
+        InlineTypeTestClass(int x, float y, Object z) {
             this.x = x;
             this.y = y;
             this.z = z;
@@ -25,11 +28,14 @@ public class BC_ifacmpeq3 extends JTTTest {
     }
 
     public static boolean test(int arg) {
+
         InlineTypeTestClass inlineTypeObject1 = new InlineTypeTestClass();
         InlineTypeTestClass inlineTypeObject2 = new InlineTypeTestClass(2, 3.0f, inlineTypeObject1);
         InlineTypeTestClass inlineTypeObject3 = new InlineTypeTestClass(3, 3.0f + 2.0f, inlineTypeObject1);
         InlineTypeTestClass inlineTypeObject4 = new InlineTypeTestClass(2, 3.0f, inlineTypeObject1);
         InlineTypeTestClass inlineTypeObject5 = new InlineTypeTestClass(2 + arg, 3.0f, inlineTypeObject1);
+        InlineTypeTestClass inlineTypeObject6 = new InlineTypeTestClass(2, 3.0f, InlineTypeTestClass.escapedObject);
+        InlineTypeTestClass inlineTypeObject7 = new InlineTypeTestClass(2, 3.0f, InlineTypeTestClass.escapedObject2);
 
         boolean result;
         if (arg == 0) {
@@ -40,6 +46,8 @@ public class BC_ifacmpeq3 extends JTTTest {
             result = inlineTypeObject4 == inlineTypeObject5;
         } else if (arg == 3) {
             result = inlineTypeObject1 == inlineTypeObject2;
+        } else if (arg == 4) {
+            result = inlineTypeObject6 == inlineTypeObject7;
         } else {
             result = inlineTypeObject3 == inlineTypeObject5;
         }
@@ -75,51 +83,37 @@ public class BC_ifacmpeq3 extends JTTTest {
 
     @Test
     public void run5() throws Throwable {
-        runTest("test", 0);
+        runTest(withoutPEA, "test", 5);
     }
 
     @Test
     public void run6() throws Throwable {
-        runTest("test", 1);
+        runTest("test", 0);
     }
 
     @Test
     public void run7() throws Throwable {
-        runTest("test", 2);
+        runTest("test", 1);
     }
 
     @Test
     public void run8() throws Throwable {
-        runTest("test", 3);
+        runTest("test", 2);
     }
 
     @Test
     public void run9() throws Throwable {
+        runTest("test", 3);
+    }
+
+    @Test
+    public void run10() throws Throwable {
         runTest("test", 4);
     }
 
-// @Test
-// public void run2() throws Throwable {
-// OptionValues options = new OptionValues(getInitialOptions(), GraalOptions.PartialEscapeAnalysis,
-// false);
-// runTest(options, "test", 1);
-// }
-//
-// @Test
-// public void run3() throws Throwable {
-// runTest("test", 1);
-// }
-//
-// @Test
-// public void run4() throws Throwable {
-// OptionValues options = new OptionValues(getInitialOptions(), GraalOptions.PartialEscapeAnalysis,
-// false);
-// runTest(options, "test", 2);
-// }
-//
-// @Test
-// public void run5() throws Throwable {
-// runTest("test", 2);
-// }
+    @Test
+    public void run11() throws Throwable {
+        runTest("test", 5);
+    }
 
 }
