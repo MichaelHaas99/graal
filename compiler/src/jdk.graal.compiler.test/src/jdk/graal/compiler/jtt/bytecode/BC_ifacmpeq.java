c8fc2e1a6ac2515a55a8b11b34f35e07c99b7dfb
MichaelHaas99 <michael.haas0612@gmail.com>
2024-10-11 11:29:24 +0200

    new test cases that check for correct speculation based on profiling information in acmp for inline types

--- compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/jtt/bytecode/BC_ifacmpeq.java
+++ compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/jtt/bytecode/BC_ifacmpeq.java
@@ -57,20 +57,19 @@ public class BC_ifacmpeq extends JTTTest {
     public static boolean test(int arg) {
         InlineTypeTestClass inlineTypeObject1 = new InlineTypeTestClass();
         InlineTypeTestClass inlineTypeObject2 = new InlineTypeTestClass();
-// InlineTypeTestClass inlineTypeObject3 = new InlineTypeTestClass(inlineTypeObject1);
-// InlineTypeTestClass inlineTypeObject4 = new InlineTypeTestClass(inlineTypeObject1);
-// InlineTypeTestClass inlineTypeObject5 = new InlineTypeTestClass(inlineTypeObject2);
+        InlineTypeTestClass inlineTypeObject3 = new InlineTypeTestClass(inlineTypeObject1);
+        InlineTypeTestClass inlineTypeObject4 = new InlineTypeTestClass(inlineTypeObject1);
+        InlineTypeTestClass inlineTypeObject5 = new InlineTypeTestClass(inlineTypeObject2);
 
-// boolean result;
-// if (arg == 0) {
-// result = inlineTypeObject1 == inlineTypeObject2;
-// } else if (arg == 1) {
-// result = inlineTypeObject3 == inlineTypeObject4;
-// } else {
-// result = inlineTypeObject4 == inlineTypeObject5;
-// }
-// return result;
-        return inlineTypeObject1 == inlineTypeObject2;
+        boolean result;
+        if (arg == 0) {
+            result = inlineTypeObject1 == inlineTypeObject2;
+        } else if (arg == 1) {
+            result = inlineTypeObject3 == inlineTypeObject4;
+        } else {
+            result = inlineTypeObject4 == inlineTypeObject5;
+        }
+        return result;
     }
 
     private static final OptionValues WITHOUT_PEA = new OptionValues(getInitialOptions(), GraalOptions.PartialEscapeAnalysis, false, GraalOptions.PrintProfilingInformation, true);
