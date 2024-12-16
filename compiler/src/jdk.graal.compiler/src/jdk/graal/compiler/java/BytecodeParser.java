4be9a024263410295f1ddc646b18317ff3e3cffa
MichaelHaas99 <michael.haas0612@gmail.com>
2024-12-16 10:56:58 +0100

    bug in instanceOf for flat arrays caused error

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/java/BytecodeParser.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/java/BytecodeParser.java
@@ -5004,6 +5004,7 @@ public abstract class BytecodeParser extends CoreProvidersDelegate implements Gr
                     if (singleType instanceof HotSpotResolvedObjectType resolvedObjectType && resolvedObjectType.isArray()) {
                         // also check against the flat array class
                         flatArrayTypeCheck = append(createInstanceOf(TypeReference.createExactTrusted(resolvedObjectType.convertToFlatArray()), object, profile));
+                        typeCheck = append(LogicNode.or(typeCheck, flatArrayTypeCheck, BranchProbabilityData.unknown()));
                     }
                     if (!typeCheck.isTautology()) {
                         SpeculationLog.Speculation speculation = mayUseTypeProfile();
@@ -5024,7 +5025,7 @@ public abstract class BytecodeParser extends CoreProvidersDelegate implements Gr
             instanceOfNode = createInstanceOf(checkedType, object, null);
             if (checkedType.getType() instanceof HotSpotResolvedObjectType resolvedObjectType && resolvedObjectType.isArray()) {
                 // also check against the flat array class
-                flatArrayTypeCheck = append(createInstanceOf(TypeReference.createExactTrusted(resolvedObjectType.convertToFlatArray()), object, profile));
+                flatArrayTypeCheck = append(createInstanceOf(TypeReference.createTrusted(graph.getAssumptions(), resolvedObjectType.convertToFlatArray()), object, null));
             }
         }
         LogicNode logicNode = genUnique(instanceOfNode);
