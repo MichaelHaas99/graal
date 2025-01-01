af9267cf5e622f76e24e5fad4d6b6ac99da5348d
MichaelHaas99 <michael.haas0612@gmail.com>
2025-01-01 18:12:10 +0100

    Moved ProjNode into a new file

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/extended/InlineTypeNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/extended/InlineTypeNode.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.graalvm.word.LocationIdentity;
 
-import jdk.graal.compiler.core.common.type.Stamp;
 import jdk.graal.compiler.core.common.type.StampFactory;
 import jdk.graal.compiler.core.common.type.TypeReference;
 import jdk.graal.compiler.graph.Node;
@@ -24,17 +23,13 @@ import jdk.graal.compiler.nodes.graphbuilderconf.GraphBuilderContext;
 import jdk.graal.compiler.nodes.memory.SingleMemoryKill;
 import jdk.graal.compiler.nodes.spi.Canonicalizable;
 import jdk.graal.compiler.nodes.spi.CanonicalizerTool;
-import jdk.graal.compiler.nodes.spi.LIRLowerable;
 import jdk.graal.compiler.nodes.spi.Lowerable;
-import jdk.graal.compiler.nodes.spi.NodeLIRBuilderTool;
 import jdk.graal.compiler.nodes.spi.VirtualizableAllocation;
 import jdk.graal.compiler.nodes.spi.VirtualizerTool;
 import jdk.graal.compiler.nodes.virtual.CommitAllocationNode;
 import jdk.graal.compiler.nodes.virtual.VirtualInstanceNode;
-import jdk.vm.ci.meta.Assumptions;
 import jdk.vm.ci.meta.DeoptimizationAction;
 import jdk.vm.ci.meta.DeoptimizationReason;
-import jdk.vm.ci.meta.JavaType;
 import jdk.vm.ci.meta.ResolvedJavaField;
 import jdk.vm.ci.meta.ResolvedJavaType;
 
@@ -115,58 +110,6 @@ public class InlineTypeNode extends FixedWithNextNode implements Lowerable, Sing
             return oopOrHub;
         }
         return this;
-    }
-
-    /**
-     * The {@link ProjNode} represents one returned value from a MultiNode. E.g. an InvokeNode which
-     * has a scalarized return can return multiple values in registers.
-     */
-    @NodeInfo(nameTemplate = "ProjNode")
-    public static class ProjNode extends ValueNode implements LIRLowerable, Canonicalizable {
-        public static final NodeClass<ProjNode> TYPE = NodeClass.create(ProjNode.class);
-
-        @Input ValueNode src;
-
-        int index;
-
-        public boolean pointsToOopOrHub() {
-            return index == 0;
-        }
-
-        protected ProjNode(NodeClass<? extends ProjNode> c, Stamp stamp) {
-            super(c, stamp);
-        }
-
-        public ProjNode(Stamp stamp, InvokeNode src, int index) {
-            this(TYPE, stamp);
-            this.src = src;
-            this.index = index;
-        }
-
-        public ProjNode(JavaType type, Assumptions assumptions, InvokeNode src, int index) {
-            this(StampFactory.forDeclaredType(assumptions, type, false).getTrustedStamp(), src, index);
-        }
-
-        // TODO: Due to the cycle with the framestate, ProjNodes can be scheduled before the
-        // InvokeNode, therefore trigger InvokeNode code generation.
-        @Override
-        public void generate(NodeLIRBuilderTool generator) {
-            ((InvokeNode) src).generate(generator);
-        }
-
-        @Override
-        public Node canonical(CanonicalizerTool tool) {
-            if (!src.getNodeClass().equals(InvokeNode.TYPE)) {
-                // Inlining can replace the InvokeNode, therefore canonicalize
-                if (pointsToOopOrHub()) {
-                    // The first ProjNode always represents the regular result
-                    return src;
-                } else {
-                    return null;
-                }
-            }
-            return this;
-        }
     }
 
 
