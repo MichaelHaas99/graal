786675409cd5b801862bed36830d6377398b3ac3
MichaelHaas99 <michael.haas0612@gmail.com>
2025-01-01 18:58:47 +0100

    Created a node type for scalarized returns

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/ReturnNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/ReturnNode.java
@@ -24,119 +24,22 @@
  */
 package jdk.graal.compiler.nodes;
 
-import static jdk.graal.compiler.core.common.type.StampFactory.objectNonNull;
 import static jdk.graal.compiler.nodeinfo.NodeCycles.CYCLES_2;
 import static jdk.graal.compiler.nodeinfo.NodeSize.SIZE_4;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jdk.graal.compiler.core.common.type.ObjectStamp;
-import jdk.graal.compiler.core.common.type.StampFactory;
-import jdk.graal.compiler.core.common.type.TypeReference;
 import jdk.graal.compiler.graph.NodeClass;
-import jdk.graal.compiler.graph.NodeInputList;
 import jdk.graal.compiler.nodeinfo.NodeInfo;
-import jdk.graal.compiler.nodes.calc.IsNullNode;
-import jdk.graal.compiler.nodes.extended.TagHubNode;
-import jdk.graal.compiler.nodes.graphbuilderconf.GraphBuilderContext;
-import jdk.graal.compiler.nodes.java.LoadFieldNode;
 import jdk.graal.compiler.nodes.memory.MemoryMapNode;
 import jdk.graal.compiler.nodes.spi.LIRLowerable;
 import jdk.graal.compiler.nodes.spi.NodeLIRBuilderTool;
-import jdk.graal.compiler.nodes.spi.Virtualizable;
-import jdk.graal.compiler.nodes.spi.VirtualizerTool;
-import jdk.graal.compiler.nodes.type.StampTool;
-import jdk.graal.compiler.nodes.virtual.VirtualObjectNode;
-import jdk.vm.ci.code.BytecodeFrame;
 import jdk.vm.ci.code.TargetDescription;
 import jdk.vm.ci.meta.JavaKind;
-import jdk.vm.ci.meta.ResolvedJavaField;
-import jdk.vm.ci.meta.ResolvedJavaType;
-import jdk.vm.ci.meta.Value;
 
 @NodeInfo(cycles = CYCLES_2, size = SIZE_4, cyclesRationale = "Restore frame + ret", sizeRationale = "Restore frame + ret")
-public final class ReturnNode extends MemoryMapControlSinkNode implements LIRLowerable, Virtualizable {
+public class ReturnNode extends MemoryMapControlSinkNode implements LIRLowerable {
 
     public static final NodeClass<ReturnNode> TYPE = NodeClass.create(ReturnNode.class);
-
-    /**
-     * For a return of a scalarized inline object {@code result} contains
-     *
-     * Null if the scalarized inline object respresents a null value
-     *
-     * A pointer to an oop if the inline object has already been allocated
-     *
-     * The address to the class hub, with bit zero set to 1, indicating a non-null scalarized inline
-     * object
-     */
     @OptionalInput ValueNode result;
-
-    @OptionalInput NodeInputList<ValueNode> scalarizedInlineType;
-
-    public static ReturnNode returnScalarized(GraphBuilderContext b, ValueNode result, ResolvedJavaType type) {
-        ArrayList<ValueNode> list = new ArrayList<>();
-
-        ResolvedJavaField[] fields = type.getInstanceFields(true);
-
-        // init
-        LogicNode init = b.add(new LogicNegationNode(b.add(new IsNullNode(result))));
-        ValuePhiNode[] phis = genScalarizedCFG(b, result, fields, init);
-        list.addAll(List.of(phis));
-        // list.add(b.add(new ConditionalNode(init, ConstantNode.forByte((byte) 1, b.getGraph()),
-        // ConstantNode.forByte((byte) 0, b.getGraph()))));
-
-        // PEA will replace oop with tagged hub if it is virtual
-        return b.add(new ReturnNode(result, list, null));
-    }
-
-    public static ValuePhiNode[] genScalarizedCFG(GraphBuilderContext b, ValueNode result, ResolvedJavaField[] fields, LogicNode init) {
-        BeginNode trueBegin = b.getGraph().add(new BeginNode());
-        BeginNode falseBegin = b.getGraph().add(new BeginNode());
-
-        b.add(new IfNode(b.add(init), trueBegin, falseBegin, ProfileData.BranchProbabilityData.unknown()));
-
-        ValueNode[] loads = new ValueNode[fields.length];
-        ValueNode[] consts = new ValueNode[fields.length];
-
-        // true branch - inline object is non-null
-
-        ValueNode nonNull = PiNode.create(result, objectNonNull(), trueBegin);
-        for (int i = 0; i < fields.length; i++) {
-            LoadFieldNode load = b.add(LoadFieldNode.create(b.getAssumptions(), nonNull, fields[i]));
-            loads[i] = load;
-            if (trueBegin.next() == null && load instanceof FixedNode)
-                trueBegin.setNext((FixedNode) load);
-        }
-        EndNode trueEnd = b.add(new EndNode());
-        if (trueBegin.next() == null)
-            trueBegin.setNext(trueEnd);
-
-        // false branch - inline object is null
-
-        for (int i = 0; i < fields.length; i++) {
-            ConstantNode load = b.add(ConstantNode.defaultForKind(fields[i].getJavaKind()));
-            consts[i] = load;
-        }
-        EndNode falseEnd = b.add(new EndNode());
-        if (falseBegin.next() == null)
-            falseBegin.setNext(falseEnd);
-
-        // falseBegin.setNext(falseEnd);
-
-        // merge
-        MergeNode merge = b.append(new MergeNode());
-        merge.setStateAfter(b.add(new FrameState(BytecodeFrame.INVALID_FRAMESTATE_BCI)));
-
-        ValuePhiNode[] phis = new ValuePhiNode[fields.length];
-        for (int i = 0; i < fields.length; i++) {
-            // TODO look at inline type plugin, use merged stamp
-            phis[i] = b.add(new ValuePhiNode(StampFactory.forDeclaredType(b.getAssumptions(), fields[i].getType(), false).getTrustedStamp(), merge, loads[i], consts[i]));
-        }
-        merge.addForwardEnd(trueEnd);
-        merge.addForwardEnd(falseEnd);
-        return phis;
-    }
 
     public ValueNode result() {
         return result;
@@ -146,30 +49,20 @@ public final class ReturnNode extends MemoryMapControlSinkNode implements LIRLow
         this(result, null);
     }
 
-    public ReturnNode(ValueNode result, ArrayList<ValueNode> scalarizedInlineType, MemoryMapNode memoryMap) {
-        this(result);
-        this.scalarizedInlineType = new NodeInputList<>(this, scalarizedInlineType);
-    }
-
     public ReturnNode(ValueNode result, MemoryMapNode memoryMap) {
         super(TYPE, memoryMap);
+        this.result = result;
+    }
+
+    protected ReturnNode(NodeClass<? extends ReturnNode> c, ValueNode result) {
+        super(c);
         this.result = result;
     }
 
     @Override
     public void generate(NodeLIRBuilderTool gen) {
         assert verifyReturn(gen.getLIRGeneratorTool().target());
-        if (scalarizedInlineType != null) {
-            // return scalarized inline typ
-            JavaKind[] stackKinds = new JavaKind[scalarizedInlineType.size()];
-            Value[] operands = new Value[scalarizedInlineType.size()];
-            for (int i = 0; i < scalarizedInlineType.size(); i++) {
-                ValueNode valueNode = scalarizedInlineType.get(i);
-                stackKinds[i] = valueNode.getStackKind();
-                operands[i] = gen.operand(valueNode);
-            }
-            gen.getLIRGeneratorTool().emitScalarizedReturn(result.getStackKind(), gen.operand(result), stackKinds, operands);
-        } else if (result == null) {
+        if (result == null) {
             gen.getLIRGeneratorTool().emitReturn(JavaKind.Void, null);
         } else {
             gen.getLIRGeneratorTool().emitReturn(result.getStackKind(), gen.operand(result));
@@ -177,9 +70,6 @@ public final class ReturnNode extends MemoryMapControlSinkNode implements LIRLow
     }
 
     private boolean verifyReturn(TargetDescription target) {
-        if (scalarizedInlineType != null) {
-            return true;
-        }
         if (graph().method() != null) {
             JavaKind actual = result == null ? JavaKind.Void : result.getStackKind();
             JavaKind expected = graph().method().getSignature().getReturnKind().getStackKind();
@@ -190,25 +80,5 @@ public final class ReturnNode extends MemoryMapControlSinkNode implements LIRLow
             assert actual == expected : "return kind doesn't match: actual " + actual + ", expected: " + expected;
         }
         return true;
-    }
-
-    @Override
-    public void virtualize(VirtualizerTool tool) {
-        // only virtualize if we have a scalarized return
-        if (scalarizedInlineType == null)
-            return;
-        ValueNode alias = tool.getAlias(result);
-        if (alias instanceof VirtualObjectNode) {
-            // TODO tag the hub at bit position 0
-            TypeReference type = StampTool.typeReferenceOrNull(result);
-            assert type != null && type.isExact() : "type should not be null for constant hub node in scalarized return";
-            ConstantNode hub = ConstantNode.forConstant(tool.getStampProvider().createHubStamp(((ObjectStamp) result.stamp(NodeView.DEFAULT))),
-                            tool.getConstantReflection().asObjectHub(type.getType()), tool.getMetaAccess());
-            tool.addNode(hub);
-            ValueNode taggedHub = new TagHubNode(hub);
-            tool.addNode(taggedHub);
-            // make sure oop stays virtual and return class object instead
-            tool.replaceFirstInput(result, taggedHub);
-        }
     }
 }
---
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/ReturnScalarizedNode.java
@@ -0,0 +1,156 @@
+package jdk.graal.compiler.nodes;
+
+import static jdk.graal.compiler.core.common.type.StampFactory.objectNonNull;
+
+import java.util.List;
+
+import jdk.graal.compiler.core.common.type.ObjectStamp;
+import jdk.graal.compiler.core.common.type.StampFactory;
+import jdk.graal.compiler.core.common.type.TypeReference;
+import jdk.graal.compiler.graph.NodeClass;
+import jdk.graal.compiler.graph.NodeInputList;
+import jdk.graal.compiler.nodeinfo.NodeInfo;
+import jdk.graal.compiler.nodes.calc.IsNullNode;
+import jdk.graal.compiler.nodes.extended.TagHubNode;
+import jdk.graal.compiler.nodes.graphbuilderconf.GraphBuilderContext;
+import jdk.graal.compiler.nodes.java.LoadFieldNode;
+import jdk.graal.compiler.nodes.spi.NodeLIRBuilderTool;
+import jdk.graal.compiler.nodes.spi.Virtualizable;
+import jdk.graal.compiler.nodes.spi.VirtualizerTool;
+import jdk.graal.compiler.nodes.type.StampTool;
+import jdk.graal.compiler.nodes.virtual.VirtualObjectNode;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.ResolvedJavaField;
+import jdk.vm.ci.meta.ResolvedJavaType;
+import jdk.vm.ci.meta.Value;
+
+/**
+ * The {@link ReturnScalarizedNode} represents a return of a nullable scalarized inline object.
+ */
+@NodeInfo(nameTemplate = "ReturnScalarized")
+public class ReturnScalarizedNode extends ReturnNode implements Virtualizable {
+    public static final NodeClass<ReturnScalarizedNode> TYPE = NodeClass.create(ReturnScalarizedNode.class);
+
+    @OptionalInput private NodeInputList<ValueNode> scalarizedInlineObject;
+
+    public ReturnScalarizedNode(ValueNode result, List<ValueNode> scalarizedInlineObject) {
+        super(TYPE, result);
+        this.scalarizedInlineObject = new NodeInputList<>(this, scalarizedInlineObject);
+    }
+
+    public ValueNode getField(int index) {
+        return scalarizedInlineObject.get(index);
+    }
+
+    public static ReturnNode create(GraphBuilderContext b, ValueNode result, ResolvedJavaType type) {
+        ResolvedJavaField[] fields = type.getInstanceFields(true);
+
+        LogicNode isInit = b.add(new LogicNegationNode(b.add(new IsNullNode(result))));
+        ValuePhiNode[] phis = genCFG(b, result, fields, isInit);
+
+        // PEA will replace oop with tagged hub if it is virtual
+        return b.add(new ReturnScalarizedNode(result, List.of(phis)));
+    }
+
+    private static FrameState searchForFrameState(FixedNode start) {
+        FrameState framestate = null;
+
+        FixedNode current = start;
+        while (framestate == null) {
+            if (current instanceof StateSplit stateSplit && stateSplit.stateAfter() != null) {
+                framestate = stateSplit.stateAfter();
+            } else {
+                current = (FixedNode) current.predecessor();
+            }
+        }
+        return framestate;
+    }
+
+    public static ValuePhiNode[] genCFG(GraphBuilderContext b, ValueNode result, ResolvedJavaField[] fields, LogicNode isInit) {
+        BeginNode trueBegin = b.getGraph().add(new BeginNode());
+        BeginNode falseBegin = b.getGraph().add(new BeginNode());
+
+        IfNode ifNode = b.add(new IfNode(b.add(isInit), trueBegin, falseBegin, ProfileData.BranchProbabilityData.unknown()));
+
+        // get a valid framestate for the merge node
+        FrameState framestate = searchForFrameState(ifNode);
+
+        ValueNode[] loads = new ValueNode[fields.length];
+        ValueNode[] consts = new ValueNode[fields.length];
+
+        // true branch - inline object is non-null, load the field values
+
+        ValueNode nonNull = PiNode.create(result, objectNonNull(), trueBegin);
+        for (int i = 0; i < fields.length; i++) {
+            LoadFieldNode load = b.add(LoadFieldNode.create(b.getAssumptions(), nonNull, fields[i]));
+            loads[i] = load;
+            if (trueBegin.next() == null)
+                trueBegin.setNext(load);
+        }
+        EndNode trueEnd = b.add(new EndNode());
+
+        // check maybe needed for empty inline objects?
+        if (trueBegin.next() == null)
+            trueBegin.setNext(trueEnd);
+
+        // false branch - inline object is null, use default values of fields
+
+        for (int i = 0; i < fields.length; i++) {
+            ConstantNode load = b.add(ConstantNode.defaultForKind(fields[i].getJavaKind()));
+            consts[i] = load;
+        }
+        EndNode falseEnd = b.add(new EndNode());
+        if (falseBegin.next() == null)
+            falseBegin.setNext(falseEnd);
+
+        // merge
+        MergeNode merge = b.append(new MergeNode());
+        merge.setStateAfter(framestate);
+
+        // produces phi nodes
+        ValuePhiNode[] phis = new ValuePhiNode[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            phis[i] = b.add(new ValuePhiNode(StampFactory.forDeclaredType(b.getAssumptions(), fields[i].getType(), false).getTrustedStamp(), merge, loads[i], consts[i]));
+        }
+
+        merge.addForwardEnd(trueEnd);
+        merge.addForwardEnd(falseEnd);
+        return phis;
+    }
+
+    @Override
+    public void generate(NodeLIRBuilderTool gen) {
+
+        // get stack kinds and operands of the scalarized inline object
+        int size = scalarizedInlineObject.size();
+        JavaKind[] stackKinds = new JavaKind[size];
+        Value[] operands = new Value[size];
+        for (int i = 0; i < size; i++) {
+            ValueNode valueNode = getField(i);
+            stackKinds[i] = valueNode.getStackKind();
+            operands[i] = gen.operand(valueNode);
+        }
+        gen.getLIRGeneratorTool().emitScalarizedReturn(result.getStackKind(), gen.operand(result), stackKinds, operands);
+    }
+
+    @Override
+    public void virtualize(VirtualizerTool tool) {
+        ValueNode alias = tool.getAlias(result);
+        if (alias instanceof VirtualObjectNode) {
+            // make sure oop stays virtual and instead return hub with bit zero set
+            TypeReference type = StampTool.typeReferenceOrNull(result);
+            assert type != null && type.isExact() : "type should not be null for constant hub node in scalarized return";
+
+            // get hub
+            ConstantNode hub = ConstantNode.forConstant(tool.getStampProvider().createHubStamp(((ObjectStamp) result.stamp(NodeView.DEFAULT))),
+                            tool.getConstantReflection().asObjectHub(type.getType()), tool.getMetaAccess());
+            tool.addNode(hub);
+
+            // set bit zero to one
+            ValueNode taggedHub = new TagHubNode(hub);
+            tool.addNode(taggedHub);
+
+            tool.replaceFirstInput(result, taggedHub);
+        }
+    }
+}
