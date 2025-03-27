4e800c3b767a0caa0ecbb24522d873a01fb5ce16
MichaelHaas99 <michael.haas0612@gmail.com>
2025-03-27 20:34:41 +0100

    Virtualize from an inline object by loading its value and creating an InlineTypeNode.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/meta/HotSpotGraphBuilderPlugins.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/meta/HotSpotGraphBuilderPlugins.java
@@ -235,7 +235,7 @@ public class HotSpotGraphBuilderPlugins {
 
         Plugins plugins = new Plugins(invocationPlugins);
         if (config.valhallaEnabled) {
-            plugins.appendNodePlugin(new InlineTypePlugin());
+            plugins.appendNodePlugin(new InlineTypePlugin(options));
         }
         plugins.appendNodePlugin(new HotSpotExceptionDispatchPlugin(config, wordTypes.getWordKind()));
         StandardGraphBuilderPlugins.registerConstantFieldLoadPlugin(plugins);
