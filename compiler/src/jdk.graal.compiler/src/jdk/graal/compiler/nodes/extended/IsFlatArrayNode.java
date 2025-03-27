7a1a97b5069160d569c38b77619b4c12c9d803b4
MichaelHaas99 <michael.haas0612@gmail.com>
2025-03-27 17:35:46 +0100

    IsFlatArrayNode and IsNullFreeArrayNode can be virtualized which kills CFG.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/extended/IsFlatArrayNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/extended/IsFlatArrayNode.java
@@ -7,18 +7,22 @@ import jdk.graal.compiler.core.common.type.StampFactory;
 import jdk.graal.compiler.graph.Node;
 import jdk.graal.compiler.graph.NodeClass;
 import jdk.graal.compiler.nodeinfo.NodeInfo;
+import jdk.graal.compiler.nodes.ConstantNode;
 import jdk.graal.compiler.nodes.FixedWithNextNode;
 import jdk.graal.compiler.nodes.ValueNode;
 import jdk.graal.compiler.nodes.spi.Canonicalizable;
 import jdk.graal.compiler.nodes.spi.CanonicalizerTool;
 import jdk.graal.compiler.nodes.spi.Lowerable;
+import jdk.graal.compiler.nodes.spi.Virtualizable;
+import jdk.graal.compiler.nodes.spi.VirtualizerTool;
+import jdk.graal.compiler.nodes.virtual.VirtualObjectNode;
 import jdk.vm.ci.meta.JavaKind;
 
 /**
  * Checks if an array is flat. Either uses a bit in the mark word or the hub.
  */
 @NodeInfo(cycles = CYCLES_8, size = SIZE_8)
-public class IsFlatArrayNode extends FixedWithNextNode implements Lowerable, Canonicalizable {
+public class IsFlatArrayNode extends FixedWithNextNode implements Lowerable, Canonicalizable, Virtualizable {
     public static final NodeClass<IsFlatArrayNode> TYPE = NodeClass.create(IsFlatArrayNode.class);
 
     @Input protected ValueNode value;
@@ -47,4 +51,14 @@ public class IsFlatArrayNode extends FixedWithNextNode implements Lowerable, Can
 
     @NodeIntrinsic
     public static native boolean isFlatArray(Object node);
+
+    @Override
+    public void virtualize(VirtualizerTool tool) {
+        ValueNode alias = tool.getAlias(value);
+        if (alias instanceof VirtualObjectNode) {
+            // flat arrays can't be virtual at the moment so this is constant true
+            // TODO: adapt if flat arrays can be virtual
+            tool.replaceWithValue(ConstantNode.forBoolean(false, graph()));
+        }
+    }
 }
