580ec664861bdfbeb37c367d4bd0614b3319d9a8
MichaelHaas99 <michael.haas0612@gmail.com>
2025-10-21 15:06:26 +0200

    Correct virtualize of InstanceOfNode. Stamp stays nullable although the virtual object itself is not nullable anymore.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/java/InstanceOfNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/java/InstanceOfNode.java
@@ -269,7 +269,7 @@ public class InstanceOfNode extends UnaryOpLogicNode implements Lowerable {
         TriState fold = tryFold(alias.stamp(NodeView.DEFAULT));
         if (fold != TriState.UNKNOWN) {
             tool.replaceWithValue(LogicConstantNode.forBoolean(fold.isTrue(), graph()));
-        } else if (alias instanceof VirtualObjectNode virtualObjectNode && !tool.isNonNull(virtualObjectNode)) {
+        } else if (alias instanceof VirtualObjectNode virtualObjectNode && !StampTool.isPointerNonNull(virtualObjectNode)) {
             // in case folding failed because the stamp was nullable retry non-null
             Stamp nonNull = StampFactory.objectNonNull().improveWith(alias.stamp(NodeView.DEFAULT));
             fold = tryFold(nonNull);
@@ -278,10 +278,14 @@ public class InstanceOfNode extends UnaryOpLogicNode implements Lowerable {
                 // null
                 LogicNode result;
                 if (fold.isTrue()) {
-                    result = new IntegerEqualsNode(tool.getNonNull(virtualObjectNode), ConstantNode.forInt(1));
-                    tool.addNode(result);
+                    if (tool.isNonNull(virtualObjectNode)) {
+                        result = LogicConstantNode.tautology(graph());
+                    } else {
+                        result = new IntegerEqualsNode(tool.getNonNull(virtualObjectNode), ConstantNode.forInt(1));
+                        tool.addNode(result);
+                    }
                 } else {
-                    result = LogicConstantNode.forBoolean(false, graph());
+                    result = LogicConstantNode.contradiction(graph());
                 }
                 tool.replaceWithValue(result);
             }
