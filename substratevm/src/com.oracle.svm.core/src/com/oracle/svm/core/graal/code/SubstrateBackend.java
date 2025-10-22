255ab801c41c10d56d7b91b94b717bfc5fcfdde4
MichaelHaas99 <michael.haas0612@gmail.com>
2025-10-23 00:26:48 +0200

    Substrate needs to override method with additional parameter.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/code/SubstrateBackend.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/code/SubstrateBackend.java
@@ -166,6 +166,10 @@ public abstract class SubstrateBackend extends Backend {
     }
 
     @Override
+    public RegisterAllocationConfig newRegisterAllocationConfig(RegisterConfig registerConfig, String[] allocationRestrictedTo, Object stub, boolean isEntryPoint) {
+        return newRegisterAllocationConfig(registerConfig, allocationRestrictedTo, stub);
+    }
+
     public RegisterAllocationConfig newRegisterAllocationConfig(RegisterConfig registerConfig, String[] allocationRestrictedTo, Object stub) {
         RegisterConfig registerConfigNonNull = registerConfig == null ? getCodeCache().getRegisterConfig() : registerConfig;
         return new RegisterAllocationConfig(registerConfigNonNull, allocationRestrictedTo);
