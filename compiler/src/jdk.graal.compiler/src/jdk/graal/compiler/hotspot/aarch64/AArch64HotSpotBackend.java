b88dd481c2602cc1d606bbd47b47758b5e0b5ac8
MichaelHaas99 <michael.haas0612@gmail.com>
2025-10-25 13:34:37 +0200

    Forgot to extend stack for aarch64.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/aarch64/AArch64HotSpotBackend.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/aarch64/AArch64HotSpotBackend.java
@@ -685,6 +685,13 @@ public class AArch64HotSpotBackend extends HotSpotHostBackend implements LIRGene
         // see MacroAssembler::extend_stack_for_inline_args in macroAssembler_aarch64.cpp
         int wordSize = 8;
         masm.stp(64, fp, lr, AArch64Address.createImmediateAddress(64, AArch64Address.AddressingMode.IMMEDIATE_PAIR_PRE_INDEXED, sp, -2 * wordSize));
+        try (ScratchRegister sc = masm.getScratchRegister()) {
+            Register scratch = sc.getRegister();
+            int spInc = ((HotSpotFrameMap) crb.frameMap).getStackIncrement();
+            // the stack increment contains the fp and lr, as we just stored them on the stack
+            // subtract it
+            masm.sub(64, sp, sp, spInc - 2 * wordSize, scratch);
+        }
     }
 
     public void emitEntryPointCode(CompilationResultBuilder crb) {
