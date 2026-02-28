3f5686adcd1c349af48ef08ae52fb54b51fc36b0
MichaelHaas99 <michael.haas0612@gmail.com>
2026-02-28 19:58:15 +0100

    Follow-up refactor.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/MultiValue.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/MultiValue.java
@@ -40,7 +40,7 @@ public interface MultiValue extends ValueNodeInterface {
 
     default ReadMultiValueNode getFieldValue(int index) {
         for (ReadMultiValueNode fieldValue : getFieldValues()) {
-            if (fieldValue.getIndex() == index + 1) {
+            if (fieldValue.getFieldIndex() == index) {
                 return fieldValue;
             }
         }
